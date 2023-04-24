2fb306f231bb8c8d4d2eed84be405d48c5f885d5
VazgenYeghiazaryan <115686860+VazgenYeghiazaryan@users.noreply.github.com>
2023-04-24 12:59:26 +0400

    aaaa

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import Actors from "../routes/Actors";
 import MoviesTopRated from "../routes/MoviesTopRated";
 import SearchResult from "../routes/SearchResult"
 
+
 const App = () => {
   return (
     <>
