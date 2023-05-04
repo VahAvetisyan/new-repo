3cfc4556c8e35fcecd7f4cd40ca901123db006c4
VazgenYeghiazaryan <115686860+VazgenYeghiazaryan@users.noreply.github.com>
2023-05-04 22:16:43 +0400

    Movie page

--- src/routes/MoviePage.js
+++ src/routes/MoviePage.js
@@ -1,80 +1,79 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import "./style/moviePage.css";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import MovieReviews from "./MoviePageAttributes/MovieReviews";
-import { LinearProgress } from "@mui/material";
-import SimilarMovies from "./MoviePageAttributes/SimilarMovies";
-import MoviesCasts from "./MoviePageAttributes/MoviesCasts";
+import React, {useCallback, useEffect, useState} from "react"
+import {useParams} from "react-router-dom"
+import "./style/moviePage.css"
+import {CircularProgressbar, buildStyles} from "react-circular-progressbar"
+import MovieReviews from "./MoviePageAttributes/MovieReviews"
+import {LinearProgress} from "@mui/material"
+import SimilarMovies from "./MoviePageAttributes/SimilarMovies"
+import MoviesCasts from "./MoviePageAttributes/MoviesCasts"
 
 export default function MoviePage() {
-  const { movieId } = useParams();
-  const [videos, setVideos] = useState([]);
-  const [movie, setMovie] = useState({});
+  const {movieId} = useParams()
+  const [videos, setVideos] = useState([])
+  const [movie, setMovie] = useState({})
   const [movieLoading, setMovieLoading] = useState(false)
   const [videoLoading, setVideoLoading] = useState(false)
 
   const getMovie = useCallback(async () => {
-    let api_key = "8cc8bb5915e1ce414955be2f44bcb790";
+    let api_key = "8cc8bb5915e1ce414955be2f44bcb790"
     setMovieLoading(true)
     let response = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}&language=en-US`
-    );
-    let jsonData = await response.json();
+    )
+    let jsonData = await response.json()
     setMovieLoading(false)
-    setMovie(jsonData);
-  }, [movieId]);
+    setMovie(jsonData)
+  }, [movieId])
 
   useEffect(() => {
     if (movieId) {
-      getMovie(movieId);
+      getMovie(movieId)
     }
-  }, [movieId]);
+  }, [movieId])
 
   const getVideos = async () => {
-    let api_key = "8cc8bb5915e1ce414955be2f44bcb790";
+    let api_key = "8cc8bb5915e1ce414955be2f44bcb790"
     setVideoLoading(true)
     let response = await fetch(
       ` https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${api_key}&language=en-US`
-    );
-    let jsonData = await response.json();
+    )
+    let jsonData = await response.json()
     setVideoLoading(false)
-    setVideos(jsonData.results.slice(0, 3));
-  };
+    setVideos(jsonData.results.slice(0, 3))
+  }
   useEffect(() => {
-    getVideos();
-  }, [movie]);
+    getVideos()
+  }, [movie])
 
-
-  if(movieLoading || videoLoading){
+  if (movieLoading || videoLoading) {
     return <LinearProgress />
   }
 
   if (!movie) {
-    return <h1>Nothing to Show</h1>;
+    return <h1>Nothing to Show</h1>
   }
 
   return (
     <div
-      id="homepage"
+      id='homepage'
       style={{
         backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
         backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
+        backgroundSize: "cover"
       }}
     >
-      <div id="homepage-poster">
+      <div id='homepage-poster'>
         <img
-          id="home-img"
+          id='home-img'
           src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          alt="movie"
+          alt='movie'
         />
-        <div id="info">
+        <div id='info'>
           <h2>{movie.title}</h2>
           <h4>Release Date: {movie.release_date}</h4>
           <h5>Discription: {movie.overview}</h5>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <div style={{ width: 50, height: 50 }}>
+          <div style={{display: "flex", alignItems: "center"}}>
+            <div style={{width: 50, height: 50}}>
               <CircularProgressbar
                 value={movie.vote_average * 10}
                 text={
@@ -90,30 +89,30 @@ export default function MoviePage() {
                   pathColor: `green`,
                   textColor: "white",
                   trailColor: "red",
-                  backgroundColor: "#d6d6d6",
+                  backgroundColor: "#d6d6d6"
                 })}
               />
             </div>
-            <p style={{ marginLeft: 20 }}>({movie.vote_count} votes)</p>
+            <p style={{marginLeft: 20}}>({movie.vote_count} votes)</p>
           </div>
         </div>
       </div>
-      <MoviesCasts id={movie.id}/>
-      <div className="video">
+      <MoviesCasts id={movie.id} />
+      <div className='video'>
         {videos.map((el) => (
           <iframe
             key={el.key}
-            width="400"
-            height="250"
+            width='400'
+            height='250'
             src={`https://www.youtube.com/embed/${el.key}`}
-            title="YouTube video player"
-            frameBorder="0"
-            allowFullScreen="allowfullscreen"
+            title='YouTube video player'
+            frameBorder='0'
+            allowFullScreen='allowfullscreen'
           ></iframe>
         ))}
       </div>
-        <SimilarMovies id={movie.id} />     
+      <SimilarMovies id={movie.id} />
       <MovieReviews id={movie.id} />
     </div>
-  );
+  )
 }
