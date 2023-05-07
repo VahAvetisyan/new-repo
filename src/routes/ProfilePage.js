62cde3c164d56da426a641baf836782f0169eeb7
moviesProjectAca <131516722+moviesProjectAca@users.noreply.github.com>
2023-05-08 02:03:20 +0400

    comments

--- src/routes/ProfilePage.js
+++ src/routes/ProfilePage.js
@@ -8,13 +8,14 @@ function Profile() {
   const [image, setImage] = useState(null);
   const [url, setUrl] = useState(null);
   const [userName, setUserName] = useState("");
+  auth.currentUser.photoURL = url;
 
   const docRef = doc(db, "Users", `${auth.lastNotifiedUid}`);
   setTimeout(() => {
     const docSnap = getDoc(docRef);
     docSnap.then((el) => {
       setUserName(el.data().username);
-      setUrl(el.data().photoUrl)
+      setUrl(el.data().photoUrl);
     });
   });
 
@@ -41,25 +42,24 @@ function Profile() {
 
   return (
     <div id="profile">
-    <div id="profile-page">
-      {auth?<img
-        id="profile-picture"
-        src={url}
-        alt="avatar"
-      />:
-      <img
-        id="profile-picture"
-        src={`https://firebasestorage.googleapis.com/v0/b/aca-project-35029.appspot.com/o/users%2Fdefault-avatar.jpg?alt=media&token=365ea4fa-30e6-42dc-aaca-876523d3158f`}
-        alt="avatar"
-      />}
-      <input type="file" name="file" onChange={handleImageChange} />
-      <button id="upload-button" onClick={handleSubmit}>
-        Upload Image
-      </button>
-    </div>
-    <div id="info">
-      <h3>Username: {userName}</h3>
-    </div>
+      <div id="profile-page">
+        {auth ? (
+          <img id="profile-picture" src={url} alt="avatar" />
+        ) : (
+          <img
+            id="profile-picture"
+            src={`https://firebasestorage.googleapis.com/v0/b/aca-project-35029.appspot.com/o/users%2Fdefault-avatar.jpg?alt=media&token=365ea4fa-30e6-42dc-aaca-876523d3158f`}
+            alt="avatar"
+          />
+        )}
+        <input type="file" name="file" onChange={handleImageChange} />
+        <button id="upload-button" onClick={handleSubmit}>
+          Upload Image
+        </button>
+      </div>
+      <div id="info">
+        <h3>Username: {userName}</h3>
+      </div>
     </div>
   );
 }
